b2ac28ef0d4178252d267320c7add6079a5eac80
Vance Lucas <vance@vancelucas.com>
2011-05-10 15:27:45 -0500

    Updated routes and configuration

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,7 +1,8 @@
 <?php
-// Your own custom routes
-// ...
-
-
 // Default routes distributed with Alloy
 require $kernel->config('alloy.path.config') . '/routes.php';
+
+
+// Override 'defualt' route to point to 'Blog' module
+$router->route('default', '/')
+    ->defaults(array('module' => 'Blog', 'action' => 'index', 'format' => 'html'));
