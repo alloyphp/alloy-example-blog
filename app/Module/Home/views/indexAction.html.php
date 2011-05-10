9ab26fa799a5440789ef180f65a573966838c04f
Vance Lucas <vance@vancelucas.com>
2011-05-10 15:29:49 -0500

    Home module removed: Not a part of the example app

--- app/Module/Home/views/indexAction.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Sets page title
-// @see app/layouts/app.html.php
-$view->head()->title('Hello from Alloy Framework!');
-?>
-
-<p><?php echo $greeting; ?>!</p>
-<p><code>Find me in: <?php echo __FILE__; ?></code></p>
